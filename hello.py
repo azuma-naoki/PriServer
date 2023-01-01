7e45943d7f86868341dcb163a237e2a43e0cbc80
naoki <naoki51522@gmail.com>
2023-01-01 22:44:47 +0900

    corsの追加

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import  matplotlib.pyplot as plt
 import numpy as np
 import base64
 from io import BytesIO
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
@@ -21,7 +23,7 @@ def test():
 
     return jsonify(books)
 
-@app.route("/print", methods=['GET'])
+@app.route("/print", methods=['POST'])
 def write():
     json = request.get_json()
 
